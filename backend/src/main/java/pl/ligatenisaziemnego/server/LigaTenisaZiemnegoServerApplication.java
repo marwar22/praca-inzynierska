f9e614ad99348a4ea5f82d6185990bd686fe6575
marwar22 <amarcinw@gmail.com>
2023-11-06 19:34:41 +0100

    Initialized backend

---
+++ backend/src/main/java/pl/ligatenisaziemnego/server/LigaTenisaZiemnegoServerApplication.java
@@ -0,0 +1,13 @@
+package pl.ligatenisaziemnego.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaTenisaZiemnegoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaTenisaZiemnegoServerApplication.class, args);
+	}
+
+}
